7267c47064e60e0e827ac0e2415200c870f203e0
Ebenezer Williams <ebenezerw@me.com>
2017-03-30 11:05:00 -0400

    done

--- src/index.js
+++ src/index.js
@@ -1,9 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import './index.css';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { Provider } from 'react-redux'
+// The Provider is a special type of component.
+// The Provider will wrap our App component along with a store that encapsulates
+// local states within the App component.
+
+import App from './components/App'
+import Store from './Store'
+
+
+const StoreInstance = Store()
 
 ReactDOM.render(
-  <App />,
+  <Provider store={StoreInstance}>
+    <App />
+  </Provider>,
   document.getElementById('root')
-);
+)
